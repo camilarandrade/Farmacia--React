f7f9accda6ca54b9a9846fa46e547f0feae8feec
Camila Andrade <camila.rda@outlook.com>
2024-08-16 11:03:29 -0300

    add components

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Footer() {
+    
+  return (
+    <>
+        <div className="flex justify-center bg-indigo-900 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
+            <p className='text-lg'>Acesse nossas redes sociais</p>
+            <div className='flex gap-2'>
+            </div>
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default Footer
